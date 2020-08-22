18ef76d40125b9301c0efe696b64fef862f69286
erinkelsey <erinszilagyi@gmail.com>
2020-08-21 21:16:09 -0600

    Add ability fetch public todos in feed and get notification when new todo added. Updated README

--- README.md
+++ README.md
@@ -1,23 +1,35 @@
-# todo_app
+## About
 
 ToDo List app for iOS, Android and Web using Flutter and Dart. Hasura hosted GraphQL backend and Auth0 for authentication.
 
-Hasura GraphiQL:
+Hasura ToDos GraphQL API: https://hasura.io/learn/graphql/graphiql?tutorial=react-native
 
-- Test username:
-- Test password:
+Hosted web example: http://todo-app-flutter.s3-website.ca-central-1.amazonaws.com
 
-Hosted web example:
+## Functionality
 
-## Getting Started
+- Basic authentication -> signup, login, logout
+- Add todos to your private list, and view all, active and completed todos
+- View online users
+- Add todo to public feed, and get notification when new todo added to feed
 
-This project is a starting point for a Flutter application.
+## Install and Setup
 
-A few resources to get you started if this is your first Flutter project:
+You will need Flutter installed, along with a number of dependencies for building and running iOS and Android apps on simulators. You can find the install instructions here: https://flutter.dev/docs/get-started/install
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+To run with web, follow the instructions here: https://flutter.dev/docs/get-started/web
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Run
+
+Run on a device (once it is connected):
+
+    $ flutter run
+
+Run on a web browser (if you have installed support for running on web):
+
+    $ flutter run -d chrome
+
+## TODO
+
+- Documentation
+- Test cases
--- lib/screens/tabs/dashboard/feeds.dart
+++ lib/screens/tabs/dashboard/feeds.dart
@@ -1,7 +1,11 @@
+import 'package:todo_app/data/feed_fetch.dart';
+
 import '../../../components/custom_button.dart';
 import '../../../components/feed_tile.dart';
 import '../../../data/feed_list.dart';
+
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class Feeds extends StatefulWidget {
   const Feeds({Key key}) : super(key: key);
@@ -11,51 +15,129 @@ class Feeds extends StatefulWidget {
 }
 
 class _FeedsState extends State<Feeds> {
+  static int _lastLatestFeedId;
+  static int _oldestFeedId;
+  static int _newTodoCount = 0;
+  static int _previousId = 0;
+  static int _newId = 0;
+  GraphQLClient _client;
   TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _client = GraphQLProvider.of(context).value;
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                constraints: BoxConstraints(minWidth: 500, maxWidth: 500),
-                child: TextFormField(
-                  controller: _controller,
-                  decoration: InputDecoration(
-                    labelText: "Say something ...",
-                    border: OutlineInputBorder(),
-                  ),
+        Mutation(
+            options:
+                MutationOptions(documentNode: gql(FeedFetch.addPublicTodo)),
+            builder: (RunMutation runMutation, QueryResult result) {
+              return Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      constraints: BoxConstraints(minWidth: 500, maxWidth: 500),
+                      child: TextFormField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          labelText: "Say something ...",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CustomButton(
+                        width: 90,
+                        height: 50,
+                        onTap: () {
+                          runMutation({"title": _controller.text});
+                          _controller.clear();
+                          FocusScope.of(context).requestFocus(new FocusNode());
+                        },
+                        text: "Post",
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CustomButton(
-                  width: 90,
-                  height: 50,
-                  onTap: () {
-                    feedList.addFeed("", "You", _controller.text);
-                    _controller.clear();
-                    FocusScope.of(context).requestFocus(new FocusNode());
-                  },
-                  text: "Post",
+              );
+            }),
+        Subscription("fetchNewNotification", FeedFetch.fetchNewNotification,
+            builder: ({bool loading, dynamic payload, dynamic error}) {
+          if (error != null) print('Error ----> $error');
+
+          if (payload != null) {
+            _newId = payload['todos'][0]['id'];
+
+            if (_previousId != 0) {
+              _newTodoCount = _newTodoCount + (_newId - _previousId);
+            } else {
+              _lastLatestFeedId = _newId;
+              _client
+                  .query(
+                QueryOptions(
+                  documentNode: gql(FeedFetch.loadMoreTodos),
+                  variables: {"oldestTodoId": _newId + 1},
                 ),
               )
-            ],
-          ),
-        ),
-        CustomButton(
-          onTap: () {
-            print("loading");
-          },
-          height: 50,
-          text: "New Notification",
-          width: MediaQuery.of(context).size.width / 2,
-        ),
+                  .then((onValue) {
+                for (var todo in onValue.data.data['todos']) {
+                  feedList.addFeed(todo['id'].toString(), todo['title'],
+                      todo['user']['name']);
+                }
+                setState(() {});
+                // feedList.addFeed(username, feed)
+              }).catchError((onError) {
+                print(onError);
+              });
+            }
+
+            _previousId = payload['todos'][0]['id'];
+
+            if (_newTodoCount != 0) {
+              return CustomButton(
+                onTap: () {
+                  // scrollController.animateTo(0.0,
+                  //     curve: Curves.easeIn,
+                  //     duration: Duration(milliseconds: 300));
+                  _client
+                      .query(
+                    QueryOptions(
+                      documentNode: gql(FeedFetch.newTodos),
+                      variables: {"latestVisibleId": _lastLatestFeedId},
+                    ),
+                  )
+                      .then((onValue) {
+                    for (var todo in onValue.data.data['todos'].reversed) {
+                      feedList.addfirstFeed(todo['id'].toString(),
+                          todo['title'], todo['user']['name']);
+                    }
+                    _lastLatestFeedId = int.parse(feedList.list.first.id);
+                    _newTodoCount = 0;
+                    setState(() {});
+                  }).catchError((onError) {
+                    print(onError);
+                  });
+                },
+                height: 50,
+                text: "$_newTodoCount New Notification",
+                width: MediaQuery.of(context).size.width / 2,
+              );
+            } else
+              return SizedBox();
+          } else
+            return SizedBox();
+        }),
         SizedBox(height: 20),
         Expanded(
           child: Container(
@@ -73,7 +155,23 @@ class _FeedsState extends State<Feeds> {
         ),
         CustomButton(
           onTap: () {
-            print("load more");
+            _oldestFeedId = int.parse(feedList.list.last.id);
+            _client
+                .query(
+              QueryOptions(
+                documentNode: gql(FeedFetch.loadMoreTodos),
+                variables: {"oldestTodoId": _oldestFeedId},
+              ),
+            )
+                .then((onValue) {
+              for (var todo in onValue.data.data['todos']) {
+                feedList.addFeed(
+                    todo['id'].toString(), todo['title'], todo['user']['name']);
+              }
+              setState(() {});
+            }).catchError((onError) {
+              print(onError);
+            });
           },
           height: 50,
           text: "Load More",
